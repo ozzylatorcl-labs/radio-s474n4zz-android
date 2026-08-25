2f381e40ff988a69e696b6589d4645d82bb74c38
ozzylatorcl-labs <ozzylatorcl@gmail.com>
2026-08-25 01:19:15 -0400

    Sube version a 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cl.radiosatanaz.radio"
         minSdk = 23
         targetSdk = 35
-        versionCode = 23
-        versionName = "2.3"
+        versionCode = 24
+        versionName = "2.4"
     }
 
     buildFeatures { compose = true }
